bf0faa118788520ecaafcb6d935ce9028b5d3cf4
smdbs <1289707349@qq.com>
2024-09-12 12:32:32 -0400

    feat: add basic routing structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { RouterModule } from "@nestjs/core";
+import { PublicModule } from "./api_public/public.module";
+import { TestModule } from "./api_public/test/test.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    PublicModule,
+    RouterModule.register([
+      {
+        path: "public",
+        module: PublicModule,
+        children: [
+          {
+            path: "test",
+            module: TestModule,
+          },
+        ],
+      },
+    ]),
+  ],
 })
 export class AppModule {}
